564ca538713f99b7e8151dd49b6f360477743597
okit-man <diopamoses@gmail.com>
2026-09-01 22:25:41 +0100

    creating html css and js product structure

---
+++ index.js
@@ -0,0 +1,47 @@
+const url = "https://www.course-api.com/javascript-store-products";
+const productDOM = document.querySelector(".product-center");
+
+const fetchProduct = async () => {
+    productDOM.innerHTML = ` <div class="spinner"></div>`;
+    try {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        // return data from fetchProduct
+        // return displayProduct(data)
+        return data
+    } catch (error) {
+        // console.log(error)
+        productDOM.innerHTML = `<p class="product-text">there was an error</p>`;
+    };
+}
+fetchProduct()
+
+const  displayProduct = (list) => {
+    // console.log(list);
+    const productList = list.map((products) => {
+        const {id} = products
+        const {title, name} = products
+        const {img} = products
+        // id, name, price, img
+        return `<a href="./product.html" class="single-product">
+                <img src="./img/car.jpg" alt="title" class="single-product-img">
+                <footer class="product-footer">
+                    <h5 class="product-name">${title}</h5>
+                    <span class="price">${name}</span>
+                </footer>
+            </a>`;
+    }).join('');
+    productDOM.innerHTML = `<div class="product-container">
+        ${productList}
+    </div>`;
+
+}
+
+const start = async() => {
+    const datas = await fetchProduct()
+    displayProduct(datas)
+    // displayProduct()
+    // console.log(fetchProduct());
+    
+};
+start()
