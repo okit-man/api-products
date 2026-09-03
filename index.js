34b335ccc5805de9d0b964dec8dcef2148dd3429
okit-man <diopamoses@gmail.com>
2026-09-03 02:20:13 +0100

    creating stages for the product and displaying product from tha api

--- index.js
+++ index.js
@@ -20,21 +20,24 @@ const  displayProduct = (list) => {
     // console.log(list);
     const productList = list.map((products) => {
         const {id} = products
-        const {title, name} = products
-        const {img} = products
+        const {name:title,price} = products.fields
+        const {url: img} = products.fields.image[0]
+        const priceFormat = price / 100
         // id, name, price, img
         return `<a href="./product.html" class="single-product">
-                <img src="./img/car.jpg" alt="title" class="single-product-img">
+                <img src="${img}" alt="${title}" class="single-product-img">
                 <footer class="product-footer">
                     <h5 class="product-name">${title}</h5>
-                    <span class="price">${name}</span>
+                    <span class="price">${priceFormat}</span>
+                    
                 </footer>
             </a>`;
-    }).join('');
+    })
+    .join('');
     productDOM.innerHTML = `<div class="product-container">
         ${productList}
     </div>`;
-
+    
 }
 
 const start = async() => {
